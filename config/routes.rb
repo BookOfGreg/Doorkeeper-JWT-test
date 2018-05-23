76e369bd2c8f48ea5cb1f11500d18ce85890e89c
Greg Myers <neonmd@hotmail.co.uk>
2018-05-24 00:15:35 +0100

    Use correct routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :api do
-    namespace :v1 do
+  scope :api do
+    scope :v1 do
       resources :users
     end
   end
